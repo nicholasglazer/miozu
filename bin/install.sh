03135187d8762f2e4b476c31e38aff290aeb1704
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-05-13 18:34:05 +0000

    install script initial

---
+++ bin/install.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+# Last updated 12 May 2023
+# Author: Nicholas Glazer <glazer.nicholas@gmail.com>
+#
+# Miozu dotfiles package varibales
+MIOZU_DIR="$HOME/code/github/miozu"
+#MIOZU_REPO_URL="https://github.com/nicholasglazer/miozu.git"
+
+# Set password once
+# TODO FIX password should be prompted only once
+read -r -s -p "Hey, you're about to install miozu environment. Enter your password: " password
+
+# Create all necessary directories in a loop
+dirs=(
+	"$HOME/.ssh"
+	"$HOME/.config/aria2"
+	"$HOME/.config/doom/themes"
+	"$HOME/.config/dunst"
+	"$HOME/.config/fish"
+	"$HOME/.config/fontconfig"
+	"$HOME/.config/nnn"
+	"$HOME/.config/nvim"
+	"$HOME/.config/rofi/colors"
+	"$HOME/.config/wezterm/colors"
+	"$HOME/.config/xmonad/xmobar"
+	"$HOME/.config/xmonad/lib/Themes"
+	"$HOME/.config/zathura"
+	"$HOME/code/github"
+	"$HOME/Documents"
+	"$HOME/Downloads"
+	"$HOME/Music"
+	"$HOME/Pictures/gifs"
+	"$HOME/Pictures/screenshots"
+	"$HOME/tmp/configs_backup"
+	"$HOME/Videos"
+)
+
+# if dir doesn't exist, create one
+for dir in "${dirs[@]}"; do
+	if [ ! -d "$dir" ]; then
+		mkdir -p "$dir"
+	fi
+done
+
+# Check if dir exists and it is a git repo
+if [ -d "$MIOZU_DIR" ] && [ -d "$MIOZU_DIR/.git" ]; then
+	cd "$MIOZU_DIR"
+	# Fetch latest changes
+	git fetch
+	if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+		git pull
+		echo "Miozu repository updated"
+	else
+		echo "Miozu repository up to date"
+	fi
+else
+	echo "Miozu repository not found at '$MIOZU_DIR'"
+	exit 1
+fi
+
+# Install req packages
+echo "$password" | paru -Syu --noconfirm --needed $(grep -v '^#' $MIOZU_DIR/bin/dependencies/required-packages.txt)
+
+# Prompt to install optional packages
+read -r -p "Do you want to install optional packages? (y/n): " choice
+if [[ $choice =~ ^[yY] ]]; then
+	# install optional packages with paru
+	paru -S --noconfirm --needed $(grep -v '^#' $MIOZU_DIR/bin/dependencies/optional-packages.txt)
+	# install doom-emacs
+	mv $HOME/.emacs.d $HOME/tmp/.emacs.d.bak
+	git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+	~/.emacs.d/bin/doom install
+	# install miozu theme for doom-emacs
+	git clone https://github.com/miozutheme/doom-miozu.git $HOME/.config/doom/themes
+	# run emacs as a systemd unit
+	systemctl --user enable emacs.service
+	systemctl --user start emacs.service
+fi
+
+declare -A dotfiles=(
+	[".config/aria2"]="$HOME/.config/aria2"
+	[".config/doom"]="$HOME/.config/doom"
+	[".config/dunst"]="$HOME/.config/dunst"
+	[".config/fish"]="$HOME/.config/fish"
+	[".config/fontconfig"]="$HOME/.config/fontconfig"
+	[".config/nvim"]="$HOME/.config/nvim"
+	[".config/rofi"]="$HOME/.config/rofi"
+	[".config/wezterm"]="$HOME/.config/wezterm"
+	[".config/xmonad/xmobar"]="$HOME/.config/xmonad/xmobar"
+	[".config/xmonad"]="$HOME/.config/xmonad"
+	[".config/zathura"]="$HOME/.config/zathura"
+	[".xinitrc"]="$HOME/.xinitrc"
+	[".Xresources"]="$HOME/.Xresources"
+	[".gitconfig"]="$HOME/.gitconfig"
+	["X11/xorg.conf.d/00-keyboard.conf"]="/etc/X11/xorg.conf.d/00-keyboard.conf"
+	["X11/xorg.conf.d/30-touchpad.conf"]="/etc/X11/xorg.conf.d/30-touchpad.conf"
+)
+
+# Create a backup dir for already existing configs
+backup_dir="$HOME/tmp/configs_backup"
+
+for file in "${!dotfiles[@]}"; do
+	dest="${dotfiles[$file]}"
+	backup="$backup_dir/$file.$(date +%Y%m%d_%H:%M.bkp)"
+	# check if file exists and it's not a symbolic link
+	if [ -e "$dest" ] && [ ! -L "$dest" ]; then
+		echo "Backing up $dest to $backup"
+		echo "$password" | mv "$dest" "$backup"
+	fi
+	# check if file is a directory
+	if [ -d "$MIOZU_DIR/$file" ]; then
+		# if yes, link all it contents to $dest
+		for f in "$MIOZU_DIR/$file"/*; do
+			ln -sf "$f" "$dest/$(basename "$f")"
+			echo "Linked $f to $dest/$(basename "$f")"
+		done
+	else
+		# if not, simply link the file
+		ln -sf "$MIOZU_DIR/$file" "$dest"
+		echo "Linked $MIOZU_DIR/$file to $dest"
+	fi
+done
+
+# Clone themes:
+# wezterm
+git clone https://github.com/miozutheme/wezterm.git $HOME/.config/wezterm/colors
+# rofi
+git clone https://github.com/miozutheme/rofi.git $HOME/.config/rofi/colors
+# xmonad
+git clone https://github.com/miozutheme/xmonad.git $HOME/.config/xmonad/lib/Themes
+
+# Find the location of the Fish shell executable with 'which'. 'chsh' to use Fish as a default shell
+chsh -s $(which fish)
+echo "Fish is now your default shell!"
+
+# Enable & start services/daemons
+# Bluetooth
+echo "$password" | sudo systemctl enable bluetooth.service
+echo "$password" | sudo systemctl start bluetooth.service
+# Set the AutoEnable option in the Bluetooth service configuration to automatically pair devices with 10 attempts each 3 sec
+sudo sed -i 's/#AutoEnable=.*/AutoEnable=true/' /etc/bluetooth/main.conf
+sudo sed -i 's/#ReconnectAttempts=.*/ReconnectAttempts=10/' /etc/bluetooth/main.conf
+sudo sed -i 's/#ReconnectIntervals=.*/ReconnectIntervals=3/' /etc/bluetooth/main.conf
+# Greenclip TODO implement rofi-greenclip later
+#systemctl --user enable greenclip.service
+#systemctl --user start greenclip.service
+
+# Recompile xmonad
+xmonad --recompile
