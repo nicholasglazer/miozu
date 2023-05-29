f7c74f27460e6234565690fe7b18c6aa1159c7bf
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-05-29 18:38:02 +0530

    add macros for js mode to insert console.log on a new line with partucal carret position

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "sans" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -42,9 +42,32 @@
 (after! counsel
   (setq counsel-rg-base-command "rg -M 240 --with-filename --no-heading --line-number --color never %s || true"))
 
+
+;; define macroses
+(defun my/console-log-macro ()
+  "Inserts a console.log statement."
+  (interactive)
+  (let ((selected-text (if (evil-visual-state-p)
+                           (buffer-substring-no-properties evil-visual-beginning evil-visual-end)
+                         "")))
+    (progn
+      (end-of-line)                                         ; Move to the end of the line
+      (newline-and-indent)                                  ; New line with indent
+      (insert "console.log(`log: :" selected-text "${}`);") ; Insert the statement
+      (end-of-line)                                         ; Move to the end of the line
+      (backward-char 4)                                     ; Move forward to be after "${"
+      (evil-insert-state)                                   ; Enter insert mode
+      )))
+
+(map! :leader
+      :desc "Insert console.log"
+      "-" #'my/console-log-macro)
+
+
 ;; Configure the lsp to use your perfered formatter
 ;; (after! lsp-haskell
 ;;   (setq lsp-haskell-formatting-provider "brittany"))
+
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
