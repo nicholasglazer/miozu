f7058ab63b0e7d612164fa80e7441dd5cf27e544
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-05-31 01:30:03 +0530

    4 new scretchpads, 2 terminals, 2 browsers

--- .config/xmonad/lib/Scratchpads.hs
+++ .config/xmonad/lib/Scratchpads.hs
@@ -7,9 +7,13 @@
 ------------------------------------------------------------------------
 module Scratchpads
   ( myScratchpads
-  , scratchTermS
-  , scratchTermB
+  , scratchTermSL
+  , scratchTermBL
+  , scratchTermSR
+  , scratchTermBR
   , scratchFM
+  , scratchWebA
+  , scratchWebB
   ) where
 
 import           Variables                      ( myTerminal )
@@ -19,68 +23,92 @@ import           XMonad.Util.NamedScratchpad
 
 myScratchpads :: [NamedScratchpad]
 myScratchpads =
-  [ NS "terminalS" spawnTermS       findTermS       manageTermS       -- Small terminal scratchpad
-  , NS "terminalB" spawnTermB       findTermB       manageTermB       -- Big terminal scratchpad
-  , NS "FM"        spawnFileManager findFileManager manageFileManager -- File manager scratchpad
-  -- , NS "mpd"       spawnMpd         findMpd          manageMpd         -- Mpd scratchpad
-  -- , NS "mixer"     spawnMixer       findMixer        manageMixer       -- Mixer scratchpad
+  [ NS "terminalSR" spawnTermSR       findTermSR      manageTermSR       -- Small terminal scratchpad
+  , NS "terminalBR" spawnTermBR       findTermBR      manageTermBR       -- Big terminal scratchpad
+  , NS "terminalBL" spawnTermBL       findTermBL      manageTermBL       -- Big terminal scratchpad
+  , NS "terminalSL" spawnTermSL       findTermSL      manageTermSL       -- Small terminal scratchpad
+  , NS "FM"         spawnFileManager  findFileManager manageFileManager  -- File manager scratchpad
+  , NS "WebA"       spawnWebA         findWebA        manageWebA         -- Chromium small scratchpad
+  , NS "WebB"       spawnWebB         findWebB        manageWebB         -- Chromium big scratchpad
   ]
 
  where
-  role        = stringProperty "WM_WINDOW_ROLE"
-  spawnTermS  = myTerminal ++ " start --class Scratchpad_Small"  -- launch terminal
-  findTermS   = className =? "Scratchpad_Small"                  -- its window will be named "Scratchpad_Small"
-  manageTermS = customFloating $ W.RationalRect l t w h          -- geometry
+  role         = stringProperty "WM_WINDOW_ROLE"
+  spawnTermSR  = myTerminal ++ " start --class Scratchpad_Small_Right"  -- launch terminal
+  findTermSR   = className =? "Scratchpad_Small_Right"                  -- its window will be named "Scratchpad_Small_Right"
+  manageTermSR = customFloating $ W.RationalRect l t w h                -- geometry
    where
     h = 0.15          -- height 15%
-    w = 0.98          -- width 98%
-    t = 0.98 - h      -- top 98%
-    l = (1 - w) / 2   -- centered left/right
+    w = 0.57          -- width 57%
+    t = 0.98 - h      -- bottom 2%
+    l = 0.99 - w      -- right 1%
 
-  spawnTermB  = myTerminal ++ " start --class Scratchpad_Big"    -- launch terminal
-  findTermB   = className =? "Scratchpad_Big"                    -- its window will be named "Scratchpad_Big"
-  manageTermB = customFloating $ W.RationalRect l t w h          -- geometry
+  spawnTermBR  = myTerminal ++ " start --class Scratchpad_Big_Right"    -- launch terminal
+  findTermBR   = className =? "Scratchpad_Big_Right"                    -- its window will be named "Scratchpad_Big_Right"
+  manageTermBR = customFloating $ W.RationalRect l t w h                -- geometry
    where
     h = 0.78         -- height 78%
     w = 0.57         -- width 57%
     t = 0.03         -- top 3%
-    l = 0.99 - w     -- left 99%
+    l = 0.99 - w     -- right 1%
 
-  spawnFileManager  = myTerminal ++ " start --class FileManager nnn" -- launch n3 file manager
-  findFileManager   = resource =? "FileManager"                      -- its window will be named "FileManager"
-  manageFileManager = customFloating $ W.RationalRect l t w h        -- geometry
+  spawnTermSL  = myTerminal ++ " start --class Scratchpad_Small_Left"  -- launch terminal
+  findTermSL   = className =? "Scratchpad_Small_Left"                  -- its window will be named "Scratchpad_Small_Left"
+  manageTermSL = customFloating $ W.RationalRect l t w h               -- geometry
+   where
+    h = 0.31         -- height 31%
+    w = 0.40         -- width 40%
+    t = 0.98 - h     -- bottom 2%
+    l = 0.01         -- left 1%
+
+  spawnTermBL  = myTerminal ++ " start --class Scratchpad_Big_Left"    -- launch terminal
+  findTermBL   = className =? "Scratchpad_Big_Left"                    -- its window will be named "Scratchpad_Big_Left"
+  manageTermBL = customFloating $ W.RationalRect l t w h               -- geometry
+   where
+    h = 0.62        -- height 62%
+    w = 0.40        -- width 40%
+    t = 0.03        -- top 3%
+    l = 0.01        -- left 1%
+
+  spawnFileManager  = myTerminal ++ " start --class FileManager nnn"   -- launch n3 file manager
+  findFileManager   = resource =? "FileManager"                        -- its window will be named "FileManager"
+  manageFileManager = customFloating $ W.RationalRect l t w h          -- geometry
    where
     h = 0.78        -- height 78%
     w = 0.98        -- width 98%
     t = 0.03        -- top 3%
     l = (1 - w) / 2 -- centered left/right
 
-  -- spawnMixer  = "urxvt -name Pulsemixer -e pulsemixer"          -- launch pulsemixer
-  -- findMixer   = resource =? "Pulsemixer"                        -- its window will be named "pulsemixer"
-  -- manageMixer = customFloating $ W.RationalRect l t w h         -- geometry:
-  --  where
-  --   h = 0.1         -- height 10%
-  --   w = 0.40        -- width  40%
-  --   t = 0.03        -- top 3%
-  --   l = 0.01        -- left 1%
+  spawnWebA  = "chromium --restore-last-session"
+  findWebA   = className =? "Chromium"
+  manageWebA = customFloating $ W.RationalRect l t w h                 -- geometry:
+   where
+    h = 0.31       -- height 31%
+    w = 0.40       -- width 40%
+    t = 0.98 - h   -- bottom 2%
+    l = 0.01       -- left 1%
 
-  -- spawnMpd  = "urxvt -name MPD -e pms"                          -- launch pmus-git
-  -- findMpd   = resource =? "MPD"                                 -- its window will be named "mpd"
-  -- manageMpd = customFloating $ W.RationalRect l t w h           -- geometry:
-  --  where
-  --   h = 0.66        -- height, 66%
-  --   w = 0.40        -- width,  40%
-  --   t = 0.15        -- top 15%
-  --   l = 0.01        -- left 1%
+  spawnWebB  = "firefox-developer-edition --new-window http://localhost:3000 --devtools"
+  findWebB   = className =? "firefoxdeveloperedition"
+  manageWebB = customFloating $ W.RationalRect l t w h                 -- geometry:
+   where
+    h = 0.78       -- height 78%
+    w = 0.57       -- width 57%
+    t = 0.03       -- top 3%
+    l = 0.99 - w   -- right 1%
 
 
-scratchTermS :: X ()
-scratchTermS = namedScratchpadAction myScratchpads "terminalS"
-scratchTermB :: X ()
-scratchTermB = namedScratchpadAction myScratchpads "terminalB"
+scratchTermSR :: X ()
+scratchTermSR = namedScratchpadAction myScratchpads "terminalSR"
+scratchTermBR :: X ()
+scratchTermBR = namedScratchpadAction myScratchpads "terminalBR"
+scratchTermSL :: X ()
+scratchTermSL = namedScratchpadAction myScratchpads "terminalSL"
+scratchTermBL :: X ()
+scratchTermBL = namedScratchpadAction myScratchpads "terminalBL"
 scratchFM :: X ()
 scratchFM = namedScratchpadAction myScratchpads "FM"
--- scratchMixer :: X ()
--- scratchMixer = namedScratchpadAction myScratchpads "mixer"
--- scratchMusic :: X ()
--- scratchMusic = namedScratchpadAction myScratchpads "mpd"
+scratchWebA :: X ()
+scratchWebA = namedScratchpadAction myScratchpads "WebA"
+scratchWebB :: X ()
+scratchWebB = namedScratchpadAction myScratchpads "WebB"
