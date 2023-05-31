ddb9db1849c83686db8915d00bf28d25669f3bfb
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-06-01 01:14:17 +0530

    variables reworked, scrot switched to maim

--- .config/xmonad/lib/Variables.hs
+++ .config/xmonad/lib/Variables.hs
@@ -1,41 +1,107 @@
-------------------------------------------------------------------------
--- Variables
-------------------------------------------------------------------------
 module Variables
-  ( myFont,
-    myBorderWidth,
-    myModMask,
-    myFocusedBorderColor,
-    myNormalBorderColor,
-    myFocusFollowsMouse,
-    myClickJustFocuses,
-    myTerminal,
-    myAltTerminal,
-    myTextEditor,
-    myBar,
-    myLaunchManager,
-    myScreenshot,
-    myScreenshotSelected,
-    myByzanz,
-  )
-where
+  ( myFont
+  , myBorderWidth
+  , myModMask
+  , myFocusedBorderColor
+  , myNormalBorderColor
+  , myFocusFollowsMouse
+  , myClickJustFocuses
+  , myTerminal
+  , myAltTerminal
+  , myTextEditor
+  , myBar
+  , myLaunchManager
+  , myDateFormat
+  , myScreenshotsDir
+  , myGifsDir
+  , myScreenshot
+  , myScreenshotSelected
+  ) where
 
-import Data.Time (formatTime, getCurrentTime)
-import Data.Time.Format (defaultTimeLocale)
-import Miozu (miozu00, miozu02)
-import XMonad (io, Dimension, KeyMask, mod4Mask)
-import XMonad.Util.Run (safeSpawn)
+import           ScreenRecorder                 ( myGifRecorder )
 
+import           Data.Time                      ( formatTime
+                                                , getCurrentTime
+                                                )
+import           Data.Time.Format               ( defaultTimeLocale )
+import           Miozu                          ( miozu00
+                                                , miozu02
+                                                )
+import           System.Process                 ( readProcess )
+import           XMonad                         ( Dimension
+                                                , KeyMask
+                                                , io
+                                                , mod4Mask
+                                                )
+
+
+-- Behavior --
+------------------------------------------------------------------------
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
---
 myModMask :: KeyMask
 myModMask = mod4Mask
+-- Whether focus follows the mouse pointer.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+-- Whether clicking on a window to focus also passes the click to the window
+myClickJustFocuses :: Bool
+myClickJustFocuses = False
+-- Default date formatting, mostly used in file names
+myDateFormat :: String
+myDateFormat = "$(date +'%s_%Y-%m-%d-%H_%M')"
 
+-- Paths --
+------------------------------------------------------------------------
+-- Default screenshot dir
+myScreenshotsDir :: String
+myScreenshotsDir = "~/Pictures/screenshots/"
+-- Default gif dir
+myGifsDir :: String
+myGifsDir = "~/Pictures/gifs/"
+-- executes the fish shell to retrieve the value of the MIOZU_DIR variable
+myMiozuDir :: IO String
+myMiozuDir = readProcess "fish" ["-c", "echo $MIOZU_DIR"] ""
+
+-- Pragrams --
+------------------------------------------------------------------------
+-- Call xmobar with its config file
+myBar :: String
+myBar = "xmobar ~/.config/xmonad/xmobar/xmobar.hs"
+-- Main terminal emulator
+myTerminal :: String
+myTerminal = "/usr/bin/wezterm"
+-- Alt reminal TODO change logic to run alt terminal if main failed
+myAltTerminal :: String
+myAltTerminal = "/usr/bin/wezterm"
+-- Run emacsclient if emacs daemon is running to avoid full emacs reload
+myTextEditor :: String
+myTextEditor = "emacsclient -nc"
+-- Application launch manager using miozu theme
+myLaunchManager :: String
+myLaunchManager = "rofi -show drun -theme ~/.config/rofi/miozu.rasi"
+-- Return the window ID of the window that the mouse cursor is currently over and make a screenshot
+myScreenshot :: String
+myScreenshot =
+  "maim -i $(xdotool getmouselocation | grep -oP 'window:\\K[0-9a-fA-F]+')"
+    ++ myScreenshotsDir
+    ++ myDateFormat
+    ++ ".png && dunstify -h string:x-dunst-stack-tag:screenshot 'Screenshot saved'"
+-- Make a screenshot of selected area also copy screenshot to a clipboard
+myScreenshotSelected :: String
+myScreenshotSelected =
+  "maim -s | tee "
+    ++ myScreenshotsDir
+    ++ myDateFormat
+    ++ ".png | xclip -selection clipboard -t image/png && dunstify -h string:x-dunst-stack-tag:screenshot 'Screenshot saved and copied to clipboard'"
+
+
+-- Styles --
+------------------------------------------------------------------------
 myFont :: String
-myFont = "xft:Hack:pixelsize=12"
+myFont = "xft:Inter:pixelsize=12"
 
 myBorderWidth :: Dimension
 myBorderWidth = 0
@@ -44,38 +110,3 @@ myFocusedBorderColor, myNormalBorderColor :: String
 myFocusedBorderColor = miozu02
 myNormalBorderColor = miozu00
 
--- Whether focus follows the mouse pointer.
-myFocusFollowsMouse :: Bool
-myFocusFollowsMouse = True
-
--- Whether clicking on a window to focus also passes the click to the window
-myClickJustFocuses :: Bool
-myClickJustFocuses = False
-
--- The preferred programs, which is used in a binding below and by certain contrib modules.
-myTerminal :: String
-myTerminal = "/usr/bin/wezterm"
-
-myAltTerminal :: String
-myAltTerminal = "/usr/bin/wezterm"
--- Run emacsclient if emacs daemon is running to avoid full emacs reload
-myTextEditor :: String
-myTextEditor = "emacsclient -nc"
-
-myBar :: String
-myBar = "/usr/bin/xmobar ~/.config/xmonad/xmobar/xmobar.hs"
-
-myLaunchManager :: String
-myLaunchManager = "rofi -show drun -theme ~/.config/rofi/miozu.rasi"
-
--- Return the window ID of the window that the mouse cursor is currently over and make a screenshot
-myScreenshot :: String
-myScreenshot = "maim -i $(xdotool getmouselocation | grep -oP 'window:\\K[0-9a-fA-F]+') ~/Pictures/screenshots/$(date +'%s_%Y-%m-%d-%H_%M').png && dunstify -h string:x-dunst-stack-tag:screenshot 'Screenshot saved'"
--- Make a screenshot of selected area also copy screenshot to a clipboard
-myScreenshotSelected :: String
-myScreenshotSelected = "maim -s | tee ~/Pictures/screenshots/$(date +'%s_%Y-%m-%d-%H_%M').png | xclip -selection clipboard -t image/png && dunstify -h string:x-dunst-stack-tag:screenshot 'Screenshot saved and copied to clipboard'"
-
--- TODO fix issue
-myByzanz =
-  ("notify-send -t 2000 'Screen recording in:' '5 seconds' && sleep 5;notify-send -t 1500 Recording && byzanz-record -v -c --duration=15 -w 1920 -h 1080 ~/Pictures/gifs/screen_" ++)
-    <$> fmap (formatTime defaultTimeLocale "%Y-%m-%d-%T_rec.gif  && notify-send 'Recorded and saved as:' 'screen_%Y-%m-%d-%T.gif'") getCurrentTime
