272dbf3fd524678823c2e73a43fd8522b1446277
Nicholas Glazer <glazer.nicholas@gmail.com>
2023-06-04 18:05:11 +0530

    simplify workspace rules

--- .config/xmonad/lib/WorkspaceRules.hs
+++ .config/xmonad/lib/WorkspaceRules.hs
@@ -13,64 +13,46 @@
 -- To match on the WM_NAME, you can use 'title' in the same way that
 -- 'className' and 'resource' are used below.
 ------------------------------------------------------------------------
-module WorkspaceRules (
-  myManageHook
-) where
+module WorkspaceRules (myManageHook) where
 
 import XMonad
-import XMonad.Actions.FloatKeys
-import XMonad.Actions.FloatSnap
-import XMonad.Actions.Navigation2D
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.Fullscreen
-import XMonad.Layout.NoBorders
-import XMonad.Layout.PerWorkspace
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Spacing
-import XMonad.Layout.ThreeColumns
-import XMonad.ManageHook
-import XMonad.Prompt
-import XMonad.Prompt.Shell
-import XMonad.Util.EZConfig
 import XMonad.Util.NamedScratchpad
-import XMonad.Util.Run
+import XMonad.ManageHook
+import XMonad.Actions.OnScreen
 import qualified XMonad.StackSet as W
-import Workspaces (webWS, emacsWS, workWS, termWS, mediaWS, socialWS, toolsWS, magicWS, privateWS)
+import Workspaces (webWS, emacsWS, workWS, termWS, mediaWS, socialWS, toolsWS, magicWS, dualWS)
 import Scratchpads (myScratchpads)
 
 myManageHook = (composeAll . concat $
   [
     [ className =? c -->  doF (W.shift webWS   )                              | c <- myClassWebShifts     ]
   , [ className =? c -->  doF (W.shift emacsWS )                              | c <- myClassEmacsShifts   ]
-  , [ className =? t --> (doF (W.view termWS   ) <+> doF (W.shift termWS   )) | t <- myClassTermShifts    ]
-  , [ className =? t --> (doF (W.view workWS   ) <+> doF (W.shift workWS   )) | t <- myClassWorkShifts    ]
-  , [ className =? t --> (doF (W.view mediaWS  ) <+> doF (W.shift mediaWS  )) | t <- myClassMediaShifts   ]
-  , [ className =? t --> (doF (W.view socialWS ) <+> doF (W.shift socialWS )) | t <- myClassSocialShifts  ]
-  , [ className =? t --> (doF (W.view toolsWS  ) <+> doF (W.shift toolsWS  )) | t <- myClassToolsShifts   ]
-  , [ className =? t --> (doF (W.view magicWS  ) <+> doF (W.shift magicWS  )) | t <- myClassMagicShifts   ]
-  , [ className =? t --> (doF (W.view privateWS) <+> doF (W.shift privateWS)) | t <- myClassPrivateShifts ]
-  , [ className =? c --> doCenterFloat                                        | c <- myClassMagicShifts   ]
-  , [ className =? c --> doFloat                                              | c <- myClassFloats        ]
-  , [ className =? c --> doFullFloat                                          | c <- myClassFloats        ]
-  , [ isDialog       --> doCenterFloat                                                                    ]
-  , [ isFullscreen   --> doFullFloat                                                                      ]
-  ]) <+> manageDocks
-     <+> fullscreenManageHook
-     <+> namedScratchpadManageHook myScratchpads
+  -- , [ className =? t --> (doF (W.view termWS   ) <+> doF (W.shift termWS   )) | t <- myClassTermShifts    ]
+  -- , [ className =? t --> (doF (W.view workWS   ) <+> doF (W.shift workWS   )) | t <- myClassWorkShifts    ]
+  -- , [ className =? t --> (doF (W.view mediaWS  ) <+> doF (W.shift mediaWS  )) | t <- myClassMediaShifts   ]
+  -- , [ className =? t --> (doF (W.view socialWS ) <+> doF (W.shift socialWS )) | t <- myClassSocialShifts  ]
+  -- , [ className =? t --> (doF (W.view toolsWS  ) <+> doF (W.shift toolsWS  )) | t <- myClassToolsShifts   ]
+  -- , [ className =? t --> (doF (W.view magicWS  ) <+> doF (W.shift magicWS  )) | t <- myClassMagicShifts   ]
+  -- , [ className =? t --> (doF (W.view dualWS   ) <+> doF (W.shift dualWS   )) | t <- myClassDualShifts    ]
+  -- , [ className =? c --> doCenterFloat                                        | c <- myClassMagicShifts   ]
+  -- , [ className =? c --> doFloat                                              | c <- myClassFloats        ]
+  -- , [ className =? c --> doFullFloat                                          | c <- myClassFloats        ]
+  --, [ isDialog       --> doCenterFloat                                                                    ]
+  --, [ isFullscreen   --> doFullFloat                                                                      ]
+  ]) <+> namedScratchpadManageHook myScratchpads
      <+> manageHook def
 
   where
-    myClassWebShifts      = ["Firefox", "Firefox Developer Edition"]
+    myClassWebShifts      = ["Firefox Beta"]
     myClassEmacsShifts    = ["Emacs"]
-    myClassWorkShifts     = ["Chromium"]
-    myClassTermShifts     = ["Termite","Konsole","uxterm","xterm"]
-    myClassMediaShifts    = ["mpv", "vlc"]
-    myClassSocialShifts   = ["TelegramDesktop"]
-    myClassToolsShifts    = [""]
-    myClassMagicShifts    = ["Wine", "Lutris"]
-    myClassPrivateShifts  = ["Tor Browser"]
-    myClassFloats         = ["Peek", "mpv"]
-    myClassFullFloats     = [""]
+    -- myClassWorkShifts     = [""]
+    -- myClassTermShifts     = ["Termite", "Konsole", "uxterm", "xterm", "", "org.wezfurlong.wezterm"]
+    -- myClassMediaShifts    = ["vlc"]
+    -- myClassSocialShifts   = ["TelegramDesktop", "discord"]
+    -- myClassToolsShifts    = [""]
+    -- myClassMagicShifts    = ["Wine", "Lutris"]
+    -- myClassDualShifts     = [""]
+    -- --myClassFloats         = ["firefox-developer-edition", "chromium", "FileManager"]
+    -- myClassFloats         = [""]
+    -- myClassFullFloats     = [""]
