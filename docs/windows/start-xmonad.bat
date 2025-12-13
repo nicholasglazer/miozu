e131ae99149c2fa480ec501bbbc3890084dc713f
ng <glazer.nicholas@gmail.com>
2025-12-13 01:44:39 +0000

    Fix WSL distro name in batch file (archlinux)

--- docs/windows/start-xmonad.bat
+++ docs/windows/start-xmonad.bat
@@ -9,7 +9,7 @@ echo Waiting for X server...
 timeout /t 2 /nobreak > nul
 
 echo Starting XMonad in WSL...
-wsl -d Arch -e bash -lc "export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0 && export LIBGL_ALWAYS_SOFTWARE=1 && startx"
+wsl -d archlinux -e bash -lc "export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0 && export LIBGL_ALWAYS_SOFTWARE=1 && startx"
 
 echo Session ended.
 pause
